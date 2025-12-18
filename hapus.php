f8d75e6a1819ddcc27ba71773da5e112461434cc
Rayfo Huda <rayfohuda9@gmail.com>
2025-12-18 11:40:45 +0700

    Delete hapus.php

--- hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("koneksi.php");
-$hapus = mysqli_query($db, "DELETE FROM mahasiswa WHERE nim = $_GET[nim]");
-if ($hapus) {
-    header("location: list.php");
-} else {
-    print "Gagal menghapus data";
-}
-?>
